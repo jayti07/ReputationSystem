a9692af3fb7e86941bf5b6d53512ab08ea24550e
Jayti Shree <jaytishree@gmail.com>
2025-03-20 15:40:17 +0530

    Update reputation.sol

--- contract/reputation.sol
+++ contract/reputation.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract ReputationSystem {
+    struct User {
+        uint256 reputation;
+        bool exists;
+    }
+
+    mapping(address => User) private users;
+    address public owner;
+
+    event ReputationUpdated(address indexed user, uint256 newReputation);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function registerUser(address _user) external onlyOwner {
+        require(!users[_user].exists, "User already registered");
+        users[_user] = User({ reputation: 0, exists: true });
+    }
+
+    function updateReputation(address _user, uint256 _newReputation) external onlyOwner {
+        require(users[_user].exists, "User not registered");
+        users[_user].reputation = _newReputation;
+        emit ReputationUpdated(_user, _newReputation);
+    }
+
+    function getReputation(address _user) external view returns (uint256) {
+        require(users[_user].exists, "User not registered");
+        return users[_user].reputation;
+    }
+}
 
